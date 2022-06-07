6cee05449926c1520ec59a4b8959b0ad6df3a813
Andrew <15331990+ahuang11@users.noreply.github.com>
2022-06-07 08:33:44 -0700

    Update task_runners.py

--- prefect_ray/task_runners.py
+++ prefect_ray/task_runners.py
@@ -1,7 +1,7 @@
 """
 Interface and implementations of the Ray Task Runner.
-[Task Runners](/concepts/task-runners/) in Prefect are
-responsible for managing the execution of Prefect task runs.
+[Task Runners](https://orion-docs.prefect.io/api-ref/prefect/task-runners/)
+in Prefect are responsible for managing the execution of Prefect task runs.
 Generally speaking, users are not expected to interact with
 task runners outside of configuring and initializing them for a flow.
 
