8c6305e6e257c06b1dcd514ca7b654d7368b4a5e
Andrew <15331990+ahuang11@users.noreply.github.com>
2022-07-26 14:43:17 -0700

    Update example to match dask

--- prefect_ray/task_runners.py
+++ prefect_ray/task_runners.py
@@ -22,12 +22,19 @@ Example:
             shout.submit(number)
 
     if __name__ == "__main__":
-        count_to(3)
+        count_to(10)
 
     # outputs
     #0
     #1
     #2
+    #3
+    #4
+    #5
+    #6
+    #7
+    #8
+    #9
     ```
 
     Switching to a `RayTaskRunner`:
@@ -48,12 +55,19 @@ Example:
             shout.submit(number)
 
     if __name__ == "__main__":
-        count_to(3)
+        count_to(10)
 
     # outputs
-    #0
+    #3
+    #7
     #2
+    #6
+    #4
+    #0
     #1
+    #5
+    #8
+    #9
     ```
 """
 
